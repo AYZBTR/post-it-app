454453f1a02c0e7f560685aab77b45739dd640d6
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-11-05 01:01:24 +0200

    created an array named as posts which stores userinfo

--- index.js
+++ index.js
@@ -3,17 +3,34 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.set(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res)=>{
-    res.send("server working well")
-})
+let posts = [
+  {
+    username: "Aayush001",
+    content: "Good Morning!"
+  },
 
-app.listen(port, ()=>{
-    console.log("Listening to port : 8080");
-})
+  {
+    username: "Daabh001",
+    content: "Good Afternoon:)"
+  },
+
+  {
+    username: "XayzbtrX",
+    content: "Welcome to Nokia"
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send("server working well");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port : 8080");
+});
